72b76fbc783c6601d6af60b3d1e6beee7733cc7f
NeocronCat <iamryanhatton@gmail.com>
2026-03-19 12:58:14 +0000

    Line follower

--- src/LineFollower.cpp
+++ src/LineFollower.cpp
@@ -5,18 +5,61 @@ LineFollower::LineFollower(int leftSensorPin, int centreSensorPin, int rightSens
     pin_sensor_l = leftSensorPin;
     pin_sensor_c = centreSensorPin;
     pin_sensor_r = rightSensorPin;
-
-    //pinMode(pin_sensor_l, INPUT);
-    pinMode(pin_sensor_c, INPUT);
-    pinMode(pin_sensor_r, INPUT);
 }
 
 LineFollowerResult LineFollower::Read(){   
-    int sensorLOut = analogRead(pin_sensor_l); //digitalRead(pin_sensor_l);
-    int sensorCOut = digitalRead(pin_sensor_c);
-    int sensorROut = digitalRead(pin_sensor_r);
+    int sensorLOut = analogRead(pin_sensor_l);
+    int sensorCOut = analogRead(pin_sensor_c);
+    int sensorROut = analogRead(pin_sensor_r);
 
     LineFollowerResult result = { sensorLOut, sensorCOut, sensorROut };
 
     return result;
+}
+
+LineFollowerResult LineFollower::AverageRead(){
+    // Could call the Read() function, but trying to separate concerns.
+    // Get an initial read 
+    int sensorLOut = analogRead(pin_sensor_l);
+    int sensorCOut = analogRead(pin_sensor_c);
+    int sensorROut = analogRead(pin_sensor_r);
+
+    // For the next 7 reads sum it up
+    for ( int i = 0; i < 7; i++ ){
+        sensorLOut += analogRead(pin_sensor_l);
+        sensorCOut += analogRead(pin_sensor_c);
+        sensorROut += analogRead(pin_sensor_r);
+    }
+
+    // Average by dividing by 8
+    sensorLOut /= 8;
+    sensorCOut /= 8;
+    sensorROut /= 8;
+
+    LineFollowerResult result = { sensorLOut, sensorCOut, sensorROut };
+
+    // Return an average of the voltage read
+    return result;
+}
+
+bool LineFollower::CheckIsOnColour(LineFollowerResult result, char desiredColour){ 
+    bool isOnColour = false;
+    switch ( desiredColour ){
+        case 'w':
+            if ( result.left < 100 || result.centre < 100 || result.right < 100 ){
+                isOnColour = true;
+            }
+            break;
+        case 'r':
+            if ( ( result.left > 100 & result.left < 400 ) || ( result.centre > 100 & result.centre < 400 ) || ( result.right > 100 & result.right < 400 )  ){
+                isOnColour = true;
+            }
+            break;
+        case 'b':
+            if ( result.left > 500 || result.centre > 500 || result.right > 500 ){
+                isOnColour = true;
+            }
+            break;
+    } 
+    return isOnColour;
 }
